0df184e960466591f84f1e0dcebc0262911aebc0
xdbted <xdbted@gmail.com>
2022-05-11 23:57:21 +0300

    result.txt ready

--- probe.py
+++ probe.py
@@ -1,8 +1,13 @@
 result = open('result.txt', 'w')
 ResultEncode = open('result-encode.txt', 'w')
+i = 0
 with open('text.txt') as f:
     text = f.read()
     text2 = [text for text in text.encode('ascii')]
     print("  ".join(map(str, text2)), file=result)
+    while i != len(text2):
+        q = i + 1
+        print(q, end="  ", file=result)
+        i += 1
 result.close()
 ResultEncode.close()
