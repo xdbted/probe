6c361852281412772648ec3ec80f6e1fadb73e6a
xdbted <xdbted@gmail.com>
2022-05-11 21:28:28 +0300

    1 версия

--- probe.py
+++ probe.py
@@ -1,3 +1,14 @@
-print('писька')
-print('Егор писька')
-open()
+# print('писька')
+# print('Егор писька')
+# text = open('text.txt')
+result = open('result.txt', 'w')
+ResultEncode = open('result-encode.txt', 'w')
+# text_bytes = text.encode('utf-8')
+# result.close()
+# text.close()
+# ResultEncode.close()
+# text = open('text.txt')
+with open('text.txt', 'r') as text:
+    for chore in text:
+        print(chore.encode('utf-8'), file=result)
+
