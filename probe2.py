c6f05ec8abeeac47811097b4a18ad81fd48ca622
xdbted <xdbted@gmail.com>
2022-05-12 23:50:32 +0300

    123

--- probe2.py
+++ probe2.py
@@ -2,13 +2,14 @@ result = open('result.txt', 'w')
 ResultEncode = open('result-encode.txt', 'w')
 i = 0
 with open('text.txt') as text:
-    text = text.read()
-    text2 = [text for text in text.encode('ascii')]
-    # print("  ".join(map(str, text2)), file=result)
-    print(text2, file=result)
-    while i != len(text2):
+    text1 = text.read()
+    for text2 in text1.encode('ascii'):
+        text3 = str(text2)
+        print("  ".join(map(str, text3)), file=result)
+    # print(text2, file=result)
+    while i != len(text3):
         q = i + 1
-        print(q, end=" ", file=result)
+        print(q, end="   ", file=result)
         i += 1
 result.close()
 with open('result.txt', 'r') as r:
