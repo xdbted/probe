3984a69d9ea22b4315f359097a186a0b6166cdce
xdbted <xdbted@gmail.com>
2022-05-13 14:48:12 +0300

    Сюда эту блядину

--- probe.py
+++ probe.py
@@ -1,17 +1,21 @@
 result = open('result.txt', 'w')
 ResultEncode = open('result-encode.txt', 'w')
 i = 0
-with open('text.txt') as text:
-    text = text.read()
+with open('text.txt') as f:
+    text = f.read()
     text2 = [text for text in text.encode('ascii')]
-    # print("  ".join(map(str, text2)), file=result)
-    print(text2, file=result)
-    while i != len(text2):
-        q = i + 1
-        print(q, end=" ", file=result)
-        i += 1
+    print("  ".join(map(str, text2)), file=result)
+    for id, val in enumerate(text2):
+        print(id + 1, end="   ", file=result)
 result.close()
-with open('result.txt', 'r') as r:
-    for g in r.readlines(1):
-
-        print(g, file=ResultEncode)
+with open('result.txt') as r:
+    tekst = r.readlines(1)
+    for tekst1 in tekst:
+        qwe = tekst1
+        qwe = qwe.split()
+        for qwe1 in qwe:
+            qwe2 = qwe1
+            qwe3 = int(qwe2)
+            qwe4 = chr(qwe3)
+            print(qwe4, end="", file=ResultEncode)
+ResultEncode.close()
